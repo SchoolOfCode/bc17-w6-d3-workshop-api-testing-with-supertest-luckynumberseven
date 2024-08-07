9729b7dfe228e1b966c0beccb20e3afaae08ea55
SuperFlyz81 <109296884+SuperFlyz81@users.noreply.github.com>
2024-08-07 16:07:08 +0100

    Ticket 3 and 4 completed

---
+++ users/users.test.js
@@ -0,0 +1,23 @@
+import { test, expect } from "vitest";
+import app from "../app.js";
+import request from "supertest";
+
+// Ticket 3
+// Barebones test
+// test("GET /api/health works");
+
+// Ticket 4
+// Goal: make a GET request with Supertest to the /api/health
+//
+// import the Express app from app.js (because it needs to be tested) ✅
+// import the function from Supertest (maybe call it `request` like the documentation did) ✅
+// within the existing test, follow the example from the documentation and:
+//    call `request` and pass in the Express app as an argument ✅
+//    send a GET request to the /api/health endpoint ✅
+//    await the overall expression and assign it to a `response` constant ✅
+//    console.log `response.body` for now to at least see a result ✅
+
+test("responds with json", async function () {
+  const response = await request(app).get("/api/health");
+  console.log(response.body);
+});
